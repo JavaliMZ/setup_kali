ccb214f6e37a3296931d2d520d329bbbd1fae381
JavaliMZ <syjulio123@gmail.com>
2025-01-18 11:56:18 +0000

    Committing changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y i3 alacritty picom flameshot rofi
 
-wget 
+
+main_url='https://raw.githubusercontent.com/JavaliMZ/setup_kali/refs/heads/main'
+
+mkdir $HOME/.config/i3 -p
+wget $main_url/config -o $HOME/.config/i3/
+wget $main_url/alternating_layouts.py -o $HOME/.config/i3/
+
+mkdir $HOME/.config/alacritty -p
+wget $main_url/alacritty.toml -o $HOME/.config/alacritty

00d9d1752ec7f5654d23e4393627a0af83eea1b9
JavaliMZ <syjulio123@gmail.com>
2025-01-20 10:56:42 +0000

    Committing changes

--- setup.sh
+++ setup.sh
@@ -11,15 +11,15 @@ main_url='https://raw.githubusercontent.com/JavaliMZ/setup_kali/main'
 mkdir -p $HOME/.config/i3
 wget -O $HOME/.config/i3/config $main_url/config
 wget -O $HOME/.config/i3/alternating_layouts.py $main_url/alternating_layouts.py
+chmod +x $HOME/.config/i3/alternating_layouts.py
 
 mkdir -p $HOME/.config/alacritty
 wget -O $HOME/.config/alacritty/alacritty.yml $main_url/alacritty.yml
 
 
+# Enable i3 to start on boot with XFCE4 DE, and disable XFCE4 Window Manager
+setxkbmap pt
+
 
 sudo apt install python3-i3ipc
 echo "Setup completed successfully!"
-
-# Enable i3 to start on boot with XFCE4 DE, and disable XFCE4 Window Manager
-setxkbmap pt
-xfce4-session-settings

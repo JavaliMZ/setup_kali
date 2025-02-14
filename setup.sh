037ec6f32c8a71da5d1f9f5f73ca747a03844586
Sylvain Júlio <60299287+JavaliMZ@users.noreply.github.com>
2025-02-14 09:05:45 +0000

    Update setup.sh - Fix and add more tools

--- setup.sh
+++ setup.sh
@@ -2,7 +2,17 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y i3 alacritty picom flameshot rofi python3-i3ipc feh
+sudo apt install -y i3 alacritty picom flameshot rofi python3-i3ipc feh seclists golang fzf
+
+go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+go install github.com/heartbeatsjp/check-tls-cert@latest
+
+$HOME/go/bin/pdtm -ia
+
+pipx install git+https://github.com/JavaliMZ/getNmapAndCensysToTable.git
+pipx install git+https://github.com/JavaliMZ/getTLSInformationToTable.git
+pipx install git+https://github.com/JavaliMZ/getIPsFromDomains.git
+
 
 sudo apt purge -y zsh-syntax-highlighting
 
@@ -14,7 +24,7 @@ wget -O $HOME/.config/i3/alternating_layouts.py $main_url/alternating_layouts.py
 chmod +x $HOME/.config/i3/alternating_layouts.py
 
 mkdir -p $HOME/.config/alacritty
-wget -O $HOME/.config/alacritty/alacritty.yml $main_url/alacritty.yml
+wget -O $HOME/.config/alacritty/alacritty.toml $main_url/alacritty.toml
 
 
 echo "Setup completed successfully!"
